83f89fc6ebff75e786fe037c8f1a966765aebf37
BDawg666 <therealbdawg@gmail.com>
2023-06-29 14:32:03 +0200

    base.lua forgot

--- base.lua
+++ base.lua
@@ -15,10 +15,11 @@ module:hook_post_require("lib/network/matchmaking/networkmatchmakingsteam", "Net
 module:hook_post_require("lib/tweak_data/playertweakdata", "PlayerTweakData")
 module:hook_post_require("lib/tweak_data/upgradestweakdata", "UpgradesTweakData")
 module:hook_post_require("lib/tweak_data/weapontweakdata", "WeaponTweakData")
-
 module:hook_post_require("lib/units/weapons/raycastweaponbase", "raycastweaponbase")
 module:hook_post_require("lib/managers/gameplaycentralmanager", "gameplaycentralmanager")
 module:hook_post_require("lib/units/civilians/logics/civilianlogicescort", "civilianlogicescort")
+module:hook_post_require("lib/managers/hudmanager", "HUDManager")
+module:hook_post_require("lib/units/player_team/teamaidamage", "TeamAIDamage")
 --module:hook_post_require("lib/units/equipment/ammo_bag/ammobagbase", "ammobagbase")
 
 module:set_update({ id = "42754", platform = "modworkshop" })
