ec105231149dd5ecfe02e45898853aa1e8b9fa01
Uziel <clauselpitoloko@gmail.com>
2023-06-30 14:35:18 +0200

    add WeaponTweakData hook

--- weapons/npc_weapons.lua
+++ weapons/npc_weapons.lua
@@ -1,3 +1,5 @@
+local WeaponTweakData = module:hook_class("WeaponTweakData")
+
 module:post_hook(WeaponTweakData, "_init_data_glock_18_npc", function(self)
 	self.glock_18_npc.usage = "mp5"
 	self.glock_18_npc.auto.fire_rate = 0.066
