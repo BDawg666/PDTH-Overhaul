71de5bdbb45808e80c4fd390ca8139e4e942d1c5
Uziel <clauselpitoloko@gmail.com>
2023-07-01 17:59:52 +0200

    Disable player sprint when carrying gold

--- tweak_data/equipmentstweakdata.lua
+++ tweak_data/equipmentstweakdata.lua
@@ -6,20 +6,22 @@ module:post_hook(EquipmentsTweakData, "init", function(self)
 		use_function_name = "use_ammo_bag",
 		quantity = 2,
 		text_id = "debug_ammo_bag",
-		description_id = "des_ammo_bag"
+		description_id = "des_ammo_bag",
 	}
 	self.doctor_bag = {
 		icon = "equipment_doctor_bag",
 		use_function_name = "use_doctor_bag",
 		quantity = 2,
 		text_id = "debug_doctor_bag",
-		description_id = "des_doctor_bag"
+		description_id = "des_doctor_bag",
 	}
 	self.sentry_gun = {
 		icon = "equipment_sentry",
 		use_function_name = "use_sentry_gun",
 		quantity = 2,
 		text_id = "debug_sentry_gun",
-		description_id = "des_sentry_gun"
+		description_id = "des_sentry_gun",
 	}
+
+	self.specials.gold_bag_equip.player_rule = true
 end)
