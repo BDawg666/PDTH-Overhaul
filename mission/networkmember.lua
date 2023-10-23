9299dbc6b2c3c4b6d70dfa820d768bae0b46f7ef
mbien1981 <shieldhater@gmail.com>
2023-10-23 12:15:21 +0200

    Prevent unnecessary equipment sync and fix a possible crash.

--- mission/networkmember.lua
+++ mission/networkmember.lua
@@ -5,7 +5,7 @@ module:post_hook(NetworkMember, "spawn_unit", function(self, spawn_point_id, is_
 	end
 
 	local equipment_id = ElementSpy and ElementSpy._synced_equipment
-	if is_drop_in and  equipment_id then
+	if is_drop_in and equipment_id and equipment_id ~= "none" then
 		managers.network:session():send_to_peer_synched(self._peer, "give_equipment", equipment_id)
 	end
 end)
