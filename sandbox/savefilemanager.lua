e6d0d0527e195fe8de66c199cf3006943e6c445b
mbien1981 <shieldhater@gmail.com>
2023-11-11 15:32:15 +0100

    use osx lib to create save instead of os.execute

--- sandbox/savefilemanager.lua
+++ sandbox/savefilemanager.lua
@@ -1,4 +1,10 @@
 local SavefileManager = module:hook_class("SavefileManager")
+
+if Global._current_save_slot then
+	SavefileManager.PROGRESS_SLOT = Global._current_save_slot
+	return
+end
+
 local fgo_save = 69
 
 do
@@ -7,7 +13,11 @@ do
 		return string.format("%s\\save%03d.sav", path, id)
 	end
 
-	os.execute(string.format('if not exist "%s" copy %s %s', save_file(fgo_save), save_file(99), save_file(fgo_save)))
+	if not osx.file_exists(save_file(fgo_save)) then
+		osx.copy_file(save_file(99), save_file(fgo_save))
+	end
 end
 
+-- apply our current selected save slot
 SavefileManager.PROGRESS_SLOT = fgo_save
+Global._current_save_slot = SavefileManager.PROGRESS_SLOT
