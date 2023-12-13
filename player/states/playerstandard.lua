c8de44d3044348f87b9e89afb04a18cc0bceb932
mbien1981 <shieldhater@gmail.com>
2023-12-13 18:53:03 +0100

    Fix ads speed multiplier crash

--- player/states/playerstandard.lua
+++ player/states/playerstandard.lua
@@ -66,15 +66,30 @@ module:hook(PlayerStandard, "_get_max_walk_speed", function(self, t)
 end)
 
 function PlayerStandard:_stance_entered(unequipped)
-	local head_stance = self._ducking and tweak_data.player.stances.default.crouched.head or tweak_data.player.stances.default.standard.head
+	local head_stance = self._ducking and tweak_data.player.stances.default.crouched.head
+		or tweak_data.player.stances.default.standard.head
 	local weapon_id
 	if not unequipped then
 		weapon_id = self._equipped_unit:base():get_name_id()
 	end
 	local stances = tweak_data.player.stances[weapon_id] or tweak_data.player.stances.default
-	local misc_attribs = self._in_steelsight and stances.steelsight or self._ducking and stances.crouched or stances.standard
-	local duration_multiplier = self._in_steelsight and 1 / tweak_data.weapon[self._name_id].ads_speed or 1
-	local new_fov = self._in_steelsight and stances.steelsight.zoom_fov and managers.user:get_setting("fov_zoom") or managers.user:get_setting("fov_standard")
-	self._camera_unit:base():clbk_stance_entered(misc_attribs.shoulders, head_stance, misc_attribs.vel_overshot, new_fov, misc_attribs.shakers, duration_multiplier)
+	local misc_attribs = self._in_steelsight and stances.steelsight
+		or self._ducking and stances.crouched
+		or stances.standard
+	local duration_multiplier = self._in_steelsight and 1
+	if weapon_id then
+		duration_multiplier = duration_multiplier / (tablex.get(tweak_data, "weapon", weapon_id, "ads_speed") or 1)
+	end
+
+	local new_fov = self._in_steelsight and stances.steelsight.zoom_fov and managers.user:get_setting("fov_zoom")
+		or managers.user:get_setting("fov_standard")
+	self._camera_unit:base():clbk_stance_entered(
+		misc_attribs.shoulders,
+		head_stance,
+		misc_attribs.vel_overshot,
+		new_fov,
+		misc_attribs.shakers,
+		duration_multiplier
+	)
 	managers.menu:set_mouse_sensitivity(self._in_steelsight and stances.steelsight.zoom_fov)
 end
