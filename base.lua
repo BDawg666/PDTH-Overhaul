c026c3cc551fe38b8fa16f66edac83d061c6e2ac
BDawg666 <therealbdawg@gmail.com>
2024-04-01 20:59:40 +0200

    Crime Boss styled Character perks
    Dallas:
    - Intimidates cops easier (less damage required, higher change);
    - 10% higher damage done;
    Hoxton:
    - 10% more accurate with all weapons;
    - 10% more armor points;
    Wolf:
    - 10% faster drill/saw fixing speeds;
    - Can use secondary weapon when downed;
    Chains:
    - 10% faster revive speed, also gets revived faster;
    - 20% more health points.

--- base.lua
+++ base.lua
@@ -25,7 +25,7 @@ module:hook("OnModuleRegistered", "load_fgo", function()
 	end
 end)
 
--- init classes
+-- init classess
 module:hook_post_require("lib/states/ingamewaitingforplayers", function(m)
 	m:post_hook(m:hook_class("IngameWaitingForPlayersState"), "at_enter", function()
 		local scripts = { "element_spy" }
